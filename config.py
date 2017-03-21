a771d3bde4e5cf072d1712ccf186c99497c6218d
Satoshi OKANO <satokano@gmail.com>
2017-03-21 14:29:26 +0900

    key, secretは消してコミット

---
+++ config.py
@@ -0,0 +1,6 @@
+
+config = {'consumer_key': "",
+          'consumer_secret': "",
+          'access_token_key': "",
+          'access_token_secret': "",
+          'screen_name_for_search': "kabao"}
